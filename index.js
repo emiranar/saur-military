8772803324069c9cf66ad3c3a234542924c571e0
emiranar <34454671+emiranar@users.noreply.github.com>
2018-07-13 21:53:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,18 @@ channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
 
 
 
+
 var arrests = schedule.scheduleJob('0 9 * * 7', function(){
   const channelarr = client.channels.get('460001047495049229');
 channelarr.send('Please update the screenshots of your arrests @everyone!');
 });
+
+var recday = 5; // saturday
+
+var arrests = schedule.scheduleJob('55 18 * * recday', function(){
+  const channelarr = client.channels.get('460001047495049229');
+channelarr.send('It is the recruitment day @everyone!');
+});
+
+
 client.login(process.env.BOT_TOKEN);
