cc345813cee0995ee6c63cb9f8e5a0d7b3f7cd7e
emiranar <34454671+emiranar@users.noreply.github.com>
2018-07-13 22:36:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -57,11 +57,20 @@ var arrests = schedule.scheduleJob('0 9 * * 7', function(){
 channelarr.send('Please update the screenshots of your arrests @everyone!');
 });
 
-var recday = 5; // saturday
+var recday = 5; 
 
-var arrests = schedule.scheduleJob('13 19 * * ' + recday +  '', function(){
-  const channelarr = client.channels.get('460001047495049229');
-channelarr.send('It is the recruitment day @everyone!');
+var recs = schedule.scheduleJob('38 19 * * 5', function(){
+  const channelrec = client.channels.get('460001047495049229');
+channelrec.send('It is the recruitment day  ' +recday+ '  @everyone!');
+
+if (recday == 5) {
+  recday += 1;
+}
+else{
+  recday -=1;
+}
+
+channelrec.send('It is the recruitment day  ' +recday+ '  @everyone!');
 });
 
 
