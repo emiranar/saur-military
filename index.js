eb59aee64c8555b7dd226e1145fc68efe42c5404
emiranar <34454671+emiranar@users.noreply.github.com>
2018-07-18 10:06:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,29 @@ var stayawake = schedule.scheduleJob('*/5 * * * *', function(){
 
 
 
+
+
+
+
+var sswakeup = schedule.scheduleJob('30 5 * * 2', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
+});
+
 var ss = schedule.scheduleJob('45 5 * * 2', function(){
   const channel = client.channels.get('331375646729306119');
 channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
+});
+
+
+
+
+
+var sawakeup = schedule.scheduleJob('45 17 * * 2', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
 });
 
 var sa = schedule.scheduleJob('45 17 * * 2', function(){
@@ -25,9 +45,27 @@ var sa = schedule.scheduleJob('45 17 * * 2', function(){
 channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
 });
 
+
+
+
+
+var pswakeup = schedule.scheduleJob('45 5 * * 4', function(){
+    const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
+});
+
 var ps = schedule.scheduleJob('45 5 * * 4', function(){
   const channel = client.channels.get('331375646729306119');
 channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
+});
+
+
+
+var pawakeup = schedule.scheduleJob('45 17 * * 4', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
 });
 
 var pa = schedule.scheduleJob('45 17 * * 4', function(){
@@ -36,9 +74,26 @@ channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
 });
 
 
+
+
+var cswakeup = schedule.scheduleJob('45 5 * * 6', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
+});
+
 var cs = schedule.scheduleJob('45 5 * * 6', function(){
   const channel = client.channels.get('331375646729306119');
 channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
+});
+
+
+
+
+var cawakeup = schedule.scheduleJob('45 17 * * 6', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
 });
 
 var ca = schedule.scheduleJob('45 17 * * 6', function(){
@@ -47,9 +102,27 @@ channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
 });
 
 
+
+
+
+var pazswakeup = schedule.scheduleJob('45 5 * * 7', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
+});
+
 var pazs = schedule.scheduleJob('45 5 * * 7', function(){
   const channel = client.channels.get('331375646729306119');
 channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
+});
+
+
+
+
+var pazawakeup = schedule.scheduleJob('45 17 * * 7', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
 });
 
 var paza = schedule.scheduleJob('45 17 * * 7', function(){
@@ -59,6 +132,14 @@ channel.send('Wake up and prepare your weapons, It\'s WZ time @everyone!');
 
 
 
+
+
+
+var arrestswakeup = schedule.scheduleJob('0 9 * * 7', function(){
+  const channelawake = client.channels.get('460001047495049229');
+  channelawake.send('I am awake, sir!');
+  console.log('I am awake, sir!');
+});
 
 var arrests = schedule.scheduleJob('0 9 * * 7', function(){
   const channelarr = client.channels.get('454180113974689813');
