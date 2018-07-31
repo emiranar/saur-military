69ab9e957db40b2ab872b1fb23e0c7a5f71ecbe4
emiranar <34454671+emiranar@users.noreply.github.com>
2018-07-31 13:06:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -147,9 +147,9 @@ var recawake = schedule.scheduleJob('45 8 * * '+ config.recday +'', function(){
   console.log('I am awake, sir!');
 });
 
-var rec = schedule.scheduleJob('0 9 * * '+ config.recday +'', function(){
-  const channelawake = client.channels.get('460001047495049229');  // BURASI DEĞİŞECEKKKK!! (DEĞİŞKEN ADI DA)
-  channelawake.send('It\'s the recruitment day @everyone!');
+var rec = schedule.scheduleJob('7 10 * * '+ config.recday +'', function(){
+  const channelrec = client.channels.get('391219156391034890');
+  channelrec.send('It\'s the recruitment day @everyone! *-testing-*');
 });
 
 
