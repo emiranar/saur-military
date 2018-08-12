7594e29b856a5a6e55692c183d2ff08e8921a328
emiranar <34454671+emiranar@users.noreply.github.com>
2018-08-12 13:03:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ var recmonday = schedule.scheduleJob('30 0 * * 1', function(){
 
 
 
-var weeklyevent = schedule.scheduleJob('0 10 * * 7', function(){ 
+var weeklyevent = schedule.scheduleJob('5 10 * * 7', function(){ 
 
 
 var XLSX = require('xlsx');
