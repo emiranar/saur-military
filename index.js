b45bb3b0fd8191e2447748c79376cd1bc02d7d39
emiranar <34454671+emiranar@users.noreply.github.com>
2018-09-14 15:20:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const prefix = require('./config.json');
+
 
 const { SOLDIER, RANK, POINTS, NAME } = require('./data.json');
 
@@ -12,10 +12,10 @@ client.on('ready', () => {
 	
 	
 	
-	client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = message.content.slice(config.prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (command === 'vote') {
