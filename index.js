3aa3c483fc4e298edf7515909a582c1790b78818
emiranar <34454671+emiranar@users.noreply.github.com>
2018-09-14 15:08:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const { prefix } = require('./config.json');
+
 const { SOLDIER, RANK, POINTS, NAME } = require('./data.json');
 
 
